c68ce7c73543c4b6ca7d78246d38c7de20009bf3
= <=>
2022-03-24 12:46:09 +0400

    sixth

---
+++ InspectorGadget/induction.rb
@@ -0,0 +1,35 @@
+def monk(arg1, arg2)
+  "Monks"
+end
+
+mo = self.method(:monk)
+puts mo
+puts monk(1, 2)
+
+def monk(arg1, *args2)
+  "Monks" + arg1 + args2.first
+end
+
+mo = self.method(:monk)
+
+puts "Arity"
+p mo.arity
+
+puts "Parameters"
+p mo.parameters
+
+class Monastery
+  def monks(arg1, arg2)
+    "Monks" + arg1 + arg2
+  end
+end
+
+mo = Monastery.new.method(:monks)
+
+puts "Receiver --"
+p mo.receiver
+
+puts
+
+puts "Owner --"
+p mo.owner
